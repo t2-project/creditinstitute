84b00ab8fa0cc78356b4d43cc7cc77a030e151a8
Sarah Sophie Stieß <st115875@stud.uni-stuttgart.de>
2021-04-21 17:07:50 +0200

    renamed everything because i always mix it up with payment x.x

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-# payment provider
+# credit institute
 
-provides payment to the payment service of the t2 store.
+dummy credit institute for the payment service of the t2 store.
 
 takes credit card info and the total amount of money and pretends to perform payment. 
-if the card's checksum is set to "bad" payment always fails and if it is set to "good" the payment always succeeds.
-if the card's checksum is set to "timeout" there will be a timeout.
-if it is neither is fails at random. either 'functional' or by a random timeout.
+
+adjust failure- / timeout rate and the delay via these http endpoints:
+
+``/timeout/{timeout}``
+``/failurerate/{rate}``
+``/timeoutrate/{rate}``
---
+++ src/main/java/de/unistuttgart/t2/creditinstitute/CreditInstituteController.java
@@ -0,0 +1,50 @@
+package de.unistuttgart.t2.creditinstitute;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class CreditInstituteController {
+	
+	@Autowired CreditInstituteService service;
+	
+	@PostMapping("/pay")
+	public void doPayment(@RequestBody PaymentData card) throws Exception {
+		service.doPayment(card);
+	}
+	
+	@GetMapping("/timeout/{timeout}")
+	public int setTimeout(@PathVariable String timeout) {
+		try {
+			service.setTimeout(Integer.valueOf(timeout));
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+		return service.getTimeout();
+	}
+	
+	@GetMapping("/failurerate/{rate}")
+	public double setFailurerate(@PathVariable String rate) {
+		try {
+			service.setFailurerate(Double.valueOf(rate));
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+		return service.getFailurerate();
+	}
+	
+	@GetMapping("/timeoutrate/{rate}")
+	public double setTimeoutrate(@PathVariable String rate) {
+		try {
+			service.setTimeoutrate(Double.valueOf(rate));
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+		return service.getTimeoutrate();
+	}
+}
