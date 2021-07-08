e96825ef176946c987345569f5c9fa9b353555be
Sarah Sophie Stieß <st115875@stud.uni-stuttgart.de>
2021-07-08 18:35:17 +0200

    added tracing, monitoring, test report...

--- src/test/java/de/unistuttgart/t2/creditinstitute/TestContext.java
+++ src/test/java/de/unistuttgart/t2/creditinstitute/TestContext.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import io.opentracing.contrib.java.spring.jaeger.starter.JaegerAutoConfiguration;
+import io.opentracing.contrib.spring.web.starter.ServerTracingAutoConfiguration;
+
 @Configuration
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = {ServerTracingAutoConfiguration.class, JaegerAutoConfiguration.class})
 @Profile("test")
 public class TestContext {
     @Bean
