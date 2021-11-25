c455f2b3e12db78e5c2206a6aec2cd3153b603a1
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2021-11-25 09:10:30 +0100

    trace with zipkin

--- src/test/java/de/unistuttgart/t2/creditinstitute/TestContext.java
+++ src/test/java/de/unistuttgart/t2/creditinstitute/TestContext.java
@@ -1,15 +1,13 @@
 package de.unistuttgart.t2.creditinstitute;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.sleuth.autoconfig.zipkin2.ZipkinAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import io.opentracing.contrib.java.spring.jaeger.starter.JaegerAutoConfiguration;
-import io.opentracing.contrib.spring.web.starter.ServerTracingAutoConfiguration;
-
 @Configuration
-@EnableAutoConfiguration(exclude = {ServerTracingAutoConfiguration.class, JaegerAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {ZipkinAutoConfiguration.class})
 @Profile("test")
 public class TestContext {
     @Bean
