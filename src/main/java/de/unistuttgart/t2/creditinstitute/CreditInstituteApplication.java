d1acdac00eab475f8dc6e08760b9813e9835bcca
delvh <leon@kske.dev>
2022-07-09 18:57:13 +0200

    Make final things final, fix warnings, remove duplicate code

--- src/main/java/de/unistuttgart/t2/creditinstitute/CreditInstituteApplication.java
+++ src/main/java/de/unistuttgart/t2/creditinstitute/CreditInstituteApplication.java
@@ -1,25 +1,23 @@
 package de.unistuttgart.t2.creditinstitute;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.*;
 import org.springframework.context.annotation.Bean;
 
 import de.unistuttgart.t2.common.BaseScan;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.*;
 import io.swagger.v3.oas.models.info.Info;
 
 /**
  * A dummy credit institute.
  * <p>
  * This credit institute provides a very fake payment.
- * 
+ *
  * @author maumau
  */
 @EnableAutoConfiguration
 @SpringBootApplication(scanBasePackageClasses = BaseScan.class)
-public class CreditInstituteApplication {
+public final class CreditInstituteApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CreditInstituteApplication.class, args);
--- src/main/java/de/unistuttgart/t2/creditinstitute/exceptions/PaymentFailedException.java
+++ src/main/java/de/unistuttgart/t2/creditinstitute/exceptions/PaymentFailedException.java
@@ -2,10 +2,12 @@ package de.unistuttgart.t2.creditinstitute.exceptions;
 
 /**
  * Indicates that a payment failed.
- * 
+ *
  * @author maumau
  */
-public class PaymentFailedException extends Exception {
+public final class PaymentFailedException extends Exception {
+
+    private static final long serialVersionUID = 1L;
 
     public PaymentFailedException(String message) {
         super(message);
