11cff86745e440f32d304481cf1bf13c6d08b4c5
David Kopp <git@davidkopp.de>
2023-11-20 11:23:06 +0100

    Upgrade to Spring Boot 3.1.5 and upgrade all dependencies

--- src/main/java/de/unistuttgart/t2/creditinstitute/CreditInstituteApplication.java
+++ src/main/java/de/unistuttgart/t2/creditinstitute/CreditInstituteApplication.java
@@ -3,6 +3,7 @@ package de.unistuttgart.t2.creditinstitute;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -27,8 +28,10 @@ public class CreditInstituteApplication {
     }
 
     @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI().components(new Components()).info(new Info().title("Creditinstitute service API")
-            .description("API of the T2-Project's creditinstitute service."));
+    public OpenAPI customOpenAPI(@Value("${info.app.version:unknown}") String version) {
+        return new OpenAPI().components(new Components()).info(new Info()
+            .title("T2 Creditinstitute service API")
+            .description("API of the T2-Project's creditinstitute service.")
+            .version(version));
     }
 }
--- src/main/java/de/unistuttgart/t2/creditinstitute/exceptions/PaymentFailedException.java
+++ src/main/java/de/unistuttgart/t2/creditinstitute/exceptions/PaymentFailedException.java
@@ -1,5 +1,7 @@
 package de.unistuttgart.t2.creditinstitute.exceptions;
 
+import java.io.Serial;
+
 /**
  * Indicates that a payment failed.
  *
@@ -7,6 +9,7 @@ package de.unistuttgart.t2.creditinstitute.exceptions;
  */
 public final class PaymentFailedException extends Exception {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     public PaymentFailedException(String message) {
